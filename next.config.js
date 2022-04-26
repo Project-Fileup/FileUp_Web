2458e0cdd1ef9275856a0b88b5cf78769665bb61
권용빈 <yiyb0603@gmail.com>
2022-04-26 22:06:37 +0900

    Chore: 파일업 아이콘 설정

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 module.exports = {
   reactStrictMode: true,
   optimizeFonts: true,
+  swcMinify: true,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
 
   experimental: {
-    swcMinify: true,
     scrollRestoration: true,
   },
 };
