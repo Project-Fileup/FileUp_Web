2ceb3a5e9fab24d6024b08adcc908d527c0f1b35
권용빈 <yiyb0603@gmail.com>
2022-04-26 22:06:14 +0900

    feature: Button, Input 베이스 컴포넌트 및 hooks 추가

---
+++ src/components/Common/Input/CancelAbleInput/index.tsx
@@ -0,0 +1,84 @@
+import {
+  useCallback,
+  memo,
+  ChangeEvent,
+  FocusEvent,
+} from 'react';
+import { CgClose } from 'react-icons/cg';
+import styled from 'styled-components';
+import useOutSideClick from '@/hooks/common/useOutSideClick';
+import isEmpty from '@/utils/is-package/isEmpty';
+import Input, { InputProps } from '..';
+
+type CancelAbleInputProps = InputProps & {}
+
+const CancelAbleInput = (props: CancelAbleInputProps): JSX.Element => {
+  const {
+    elementRef: inputRef,
+    isFocus,
+    setIsFocus,
+  } = useOutSideClick<HTMLInputElement>();
+
+  const onReset = useCallback((): void => {
+    if (!inputRef.current || isEmpty(props.value)) {
+      return;
+    }
+
+    props.onChange!({
+      target: {
+        name: props.name,
+        value: '',
+      },
+    } as ChangeEvent<HTMLInputElement>);
+
+    inputRef.current.focus();
+  }, [inputRef, props]);
+
+  const onBlur = useCallback((e?: FocusEvent<HTMLInputElement>): void => {
+    if (e?.relatedTarget !== null) {
+      setIsFocus(false);
+    }
+  }, [setIsFocus]);
+
+  return (
+    <CancelAbleInputWrapper
+      className={props.className}
+      isFocus={isFocus}
+      margin={props.margin}
+    >
+      <Input
+        {...props}
+        margin='0'
+        inputRef={inputRef}
+        onFocus={() => setIsFocus(true)}
+        onBlur={onBlur}
+      />
+
+      <ResetInput
+        onClick={onReset}
+      />
+    </CancelAbleInputWrapper>
+  );
+}
+
+type CancelAbleInputWrapperProps = {
+  isFocus: boolean;
+  margin?: string;
+}
+
+const CancelAbleInputWrapper = styled.div<CancelAbleInputWrapperProps>`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: ${({ margin }) => margin};
+  border: ${({ theme }) => `1px solid ${theme.color.main}`};
+`;
+
+const ResetInput = styled(CgClose)`
+  color: ${({ theme }) => theme.color.main};
+  font-size: ${({ theme }) => theme.fontSize.BIG};
+  cursor: pointer;
+`;
+
+export default memo(CancelAbleInput);
